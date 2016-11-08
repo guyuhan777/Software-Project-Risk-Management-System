82fa7770ce594025ca68ffc70828e89aabf07739
wangzehao <wzhkun@163.com>
2016-11-08 13:09:32 +0800

    init project

---
+++ src/main/java/sprms/SoftwareProjectRiskManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package sprms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareProjectRiskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareProjectRiskManagementSystemApplication.class, args);
+	}
+}

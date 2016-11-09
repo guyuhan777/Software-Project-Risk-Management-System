e362ad53ff285828f53f11b16466f665a291eaf2
wangzehao <wzhkun@163.com>
2016-11-09 18:34:41 +0800

    。

--- src/main/java/sprms/service/RiskService.java
+++ src/main/java/sprms/service/RiskService.java
@@ -2,10 +2,13 @@ package sprms.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import sprms.model.Risk;
 import sprms.model.RiskState;
 import sprms.model.User;
 
+@Service
 public interface RiskService {
 	Risk submitRisk(Risk risk,User submitter);
 	Risk followRisk(Risk risk,User follower);

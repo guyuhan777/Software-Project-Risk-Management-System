dd6006a3caff27c304f3f5b5478c282e06bd0861
wangzehao <wzhkun@163.com>
2016-11-09 18:54:04 +0800

    增加User Service实现

---
+++ src/main/java/sprms/component/UserComponent.java
@@ -0,0 +1,26 @@
+package sprms.component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import sprms.model.User;
+import sprms.repository.UserRepository;
+import sprms.service.UserService;
+
+@Component
+public class UserComponent implements UserService{
+
+	@Autowired
+	UserRepository repository;
+	
+	@Override
+	public User addUser(User user) {
+		return repository.save(user);
+	}
+
+	@Override
+	public Iterable<User> getAllUser() {
+		return repository.findAll();
+	}
+
+}

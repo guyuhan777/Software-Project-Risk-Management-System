dcfe8199e7f3bceb9e206e96ba95f6847fd8ab2b
wangzehao <wzhkun@163.com>
2016-11-15 09:40:51 +0800

    1

---
+++ src/main/java/sprms/repository/RiskManagementPlanRepository.java
@@ -0,0 +1,9 @@
+package sprms.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import sprms.model.RiskManagementPlan;
+
+public interface RiskManagementPlanRepository extends CrudRepository<RiskManagementPlan,Long>{
+
+}
